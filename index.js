6a1de6efd10d644ca3d431e0fb665ed6a15cb139
Ervin543 <ervindasratt@gmail.com>
2023-03-03 22:02:50 -0600

    added code to the index and makeHTML files

--- index.js
+++ index.js
@@ -4,165 +4,134 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
-const generateHTML = require('./generateHTML');
-
-
-
-
+const makeHTML = require('./makeHTML');
 
 const teamMembers = [];
 const idArray = [];
 
 function promptAddTeamMember() {
-    return inquirer.prompt([
-      {
-        type: 'list',
-        name: 'teamMemberType',
-        message: 'Which type of team member would you like to add?',
-        choices: ['Manager', 'Engineer', 'Intern', 'None - I am done adding team members']
-      }
-    ])
-    .then(answer => {
-      switch (answer.teamMemberType) {
-        case 'Manager':
-          
-          promptManager();
-          break;
-        case 'Engineer':
-          
-          promptEngineer();
-          break;
-        case 'Intern':
-          
-          promptIntern();
-          break;
-        case 'None - I am done adding team members':
-          
-          createHTML();
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'teamMemberType',
+      message: 'Which type of team member would you like to add?',
+      choices: ['Manager', 'Engineer', 'Intern', 'None - I am done adding team members']
+    }
+  ])
+  .then(answer => {
+    switch (answer.teamMemberType) {
+      case 'Manager':
+        promptManager();
+        break;
+      case 'Engineer':
+        promptEngineer();
+        break;
+      case 'Intern':
+        promptIntern();
+        break;
+      case 'None - I am done adding team members':
+        createHTML();
+        break;
+    }
+  });
+}
 
-          break;
-      }
-    });
-  }
+let managerAnswers;
+let engineerAnswers;
+let internAnswers;
+
+function promptManager() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the Manager's name?",
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the Manager's ID?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the Manager's email address?",
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "What is the Manager's office number?",
+    },
+  ])
+  .then(answers => {
+    managerAnswers = answers;
+    const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+    teamMembers.push(manager);
+    promptAddTeamMember();
+  });
+}
+
+function promptEngineer() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the Engineer's id?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the Engineer's email address?",
+    },
+    {
+      type: 'input',
+      name: 'GitHub',
+      message: "What is the Engineer's GitHub username?",
+    },
+  ])
+  .then(answers => {
+    engineerAnswers = answers;
+    const engineer = new Engineer(engineerAnswers.id, engineerAnswers.email, engineerAnswers.GitHub);
+    teamMembers.push(engineer);
+    promptAddTeamMember();
+  });
+}
+
+function promptIntern() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the Intern's ID?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the Intern's email address?",
+    },
+    {   
+      type: 'input',
+      name: 'school',
+      message: "What is the Intern's school?",
+     },
+  ])
+  .then(answers => {
+    internAnswers = answers;
+    const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+    teamMembers.push(intern);
+    promptAddTeamMember();
+  });
+}
+
+function createHTML() {
   
 
+  const outputHtml = makeHTML(teamMembers);
+  fs.writeFile('output/team.html', outputHtml, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  );
+}
 
-  let managerAnswers;
-  let engineerAnswers;
-  let internAnswers;
-  
-  function promptManager() {
-      return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: "What is the Manager's name?",
-        },
-        {
-          type: 'input',
-          name: 'id',
-          message: "What is the Manager's ID?",
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: "What is the Manager's email address?",
-        },
-        {
-          type: 'input',
-          name: 'officeNumber',
-          message: "What is the Manager's office number?",
-        },
-      ])
-      .then(answers => {
-        managerAnswers = answers;
-        promptAddTeamMember();
-      });
-  };
-  
-  function promptEngineer() {
-      return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'id',
-          message: "What is the Engineer's id?",
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: "What is the Engineer's email address?",
-        },
-        {
-          type: 'input',
-          name: 'GitHub',
-          message: "What is the Engineer's GitHub username?",
-        },
-      ])
-      .then(answers => {
-        engineerAnswers = answers;
-        promptAddTeamMember();
-      });
-  }
-  
-  function promptIntern() {
-      return inquirer.prompt([
-        {
-          type: 'input',
-          name: 'id',
-          message: "What is the Intern's ID?",
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: "What is the Intern's email address?",
-        },
-        {
-          type: 'input',
-          name: 'school',
-          message: "What school did the Intern attend?",
-        },
-      ])
-      .then(answers => {
-        internAnswers = answers;
-        promptAddTeamMember();
-      });
-  };
-  
-  function generateHTML(teamMembers) {
-      const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
-      const engineer = new Engineer(engineerAnswers.id, engineerAnswers.email, engineerAnswers.GitHub);
-      const intern = new Intern(internAnswers.id, internAnswers.email, internAnswers.school);
-  
+promptAddTeamMember();
 
-  };
-  
-  
-  
-  
-  function createHTML(teamMembers) {
-    const filePath = path.join(__dirname, 'output', 'team.html');
-    const htmlContent = generateHTML(managerAnswers, engineerAnswers, internAnswers);
-    
-    fs.writeFile(filePath, htmlContent, err => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(`HTML file created at ${filePath}`);
-    });
-  };
-
-
-
-
-
-
-
-
-
-
-  
-  
-
-
-  
+     
---
+++ src/makeHTML.js
@@ -0,0 +1,71 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+function makeHTML(teamMembers) {
+  const cardsHtml = teamMembers.map((member) => {
+    if (member instanceof Manager) {
+      return `
+        <div class="card">
+          <h2>${member.getName()}</h2>
+          <p>${member.getRole()}</p>
+          <ul>
+            <li>ID: ${member.getId()}</li>
+            <li>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></li>
+            <li>Office Number: ${member.getOfficeNumber()}</li>
+          </ul>
+        </div>
+      `;
+    } else if (member instanceof Engineer) {
+      return `
+        <div class="card">
+          <h2>${member.getName()}</h2>
+          <p>${member.getRole()}</p>
+          <ul>
+            <li>ID: ${member.getId()}</li>
+            <li>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></li>
+            <li>GitHub: <a href="https://github.com/${member.getGitHub()}" target="_blank">${member.getGitHub()}</a></li>
+          </ul>
+        </div>
+      `;
+    } else if (member instanceof Intern) {
+      return `
+        <div class="card">
+          <h2>${member.getName()}</h2>
+          <p>${member.getRole()}</p>
+          <ul>
+            <li>ID: ${member.getId()}</li>
+            <li>Email: <a href="mailto:${member.getEmail()}">${member.getEmail()}</a></li>
+            <li>School: ${member.getSchool()}</li>
+          </ul>
+        </div>
+      `;
+    }
+  }).join('');
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha512-7hJ4UrE4NcN9XK0NioV7SrtY/YCFMKV7Q2wzKuM7c4zfG15uBcP9jFkxDhOrCpFtJElzjJPnGvHwwAdnp/R2bQ==" crossorigin="anonymous" />
+    </head>
+    <body>
+      <header class="bg-danger text-white mb-3 p-3 text-center">
+        <h1>My Team</h1>
+      </header>
+
+      <div class="container">
+        <div class="row">
+          ${cardsHtml}
+        </div>
+      </div>
+      
+    </body>
+    </html>
+  `;
+}
+
+module.exports = makeHTML;
